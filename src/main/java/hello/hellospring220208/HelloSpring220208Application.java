416f6c24b63ce4fc485e677032dabae32cfdf9b7
donaldlee <aig2029@naver.com>
2022-02-08 21:30:58 +0900

    220208 hello-spring

---
+++ src/main/java/hello/hellospring220208/HelloSpring220208Application.java
@@ -0,0 +1,12 @@
+package hello.hellospring220208;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpring220208Application {
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpring220208Application.class, args);
+	}
+
+}
---
+++ src/test/java/hello/hellospring220208/HelloSpring220208ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellospring220208;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpring220208ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
